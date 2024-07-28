b1ce4dc575ab20379329cc6f8e0e216f80cda5e8
Kaue Santana <134945789+Kaue678@users.noreply.github.com>
2024-07-28 18:31:06 -0300

    Update Regression_model.py
    Few changes to correct some issues

--- Regression_model.py
+++ Regression_model.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 data = pd.read_csv("molecular_descriptors.csv")
 
 # Extract the independent variables (X) and dependent variable (y)
-# Use the alias to X and Y axes: 'MolWt', 'logP', 'logD_7.4', 'HBA', 'HBD', 'NumRotBonds', 'NumAromaticRings', 'NumOxygenAtoms', 'NumNitrogenAtoms', 'TopologicalSurfaceArea', 'FractionSP3Carbons'
+# Use the alias to X and Y axes: 'MolWt', 'logP', 'logD_7.4', 'HBA', 'HBD', 'RTB', 'AR', 'O', 'N', 'tPSA', 'FSp3'
 
 X = data[['logP']] # Replace 'Actual_Column_Name' with the actual column name of the target variables
-y = data['TopologicalSurfaceArea']  # Replace 'Actual_Column_Name' with the actual column name of the target variables
+y = data['tPSA']  # Replace 'Actual_Column_Name' with the actual column name of the target variables
 
 # Create a linear regression model
 model = LinearRegression()
@@ -22,8 +22,8 @@ predictions = model.predict(X)
 # Plot the actual vs. predicted values
 plt.figure(figsize=(10, 6))
 plt.scatter(y, predictions)
-plt.xlabel('VAriable X')
-plt.ylabel('Variable Y')
+plt.xlabel('logP')
+plt.ylabel('tPSA')
 plt.title('Actual vs. Predicted values')
 plt.grid(True)
 plt.savefig('linear_regression_plot.png')  # Save the plot as a PNG file
